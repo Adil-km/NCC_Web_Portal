ef0013790a18450daa79c735d077d19a4c1853ad
adilkm123 <adilkmschool@gmail.com>
2025-11-21 11:12:00 +0530

    basic page navigation

--- HomePage/urls.py
+++ HomePage/urls.py
@@ -4,5 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.homePage, name="home" ),
+    path('', views.home, name="home" ),
+    path('about/', views.about, name="about" ),
+    path('join/', views.callToAction, name="join" ),
+    path('gallery/', views.gallery, name="gallery" ),
+    path('achievements/', views.achievements, name="achievements" ),
+    path('contact/', views.contact, name="contact" ),
+    path('event/', views.event, name="event" ),
+    path('login/', views.login, name="login" ),
 ]
--- HomePage/views.py
+++ HomePage/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def homePage (request):
-    return render(request, "base.html")
+def home(request):
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def achievements(request):
+    return render(request, "achievements.html")
+
+def event(request):
+    return render(request, "event.html")
+
+def gallery(request):
+    return render(request, "gallery.html")
+
+def callToAction(request):
+    return render(request, "callToAction.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def login(request):
+    return render(request, "login.html")
